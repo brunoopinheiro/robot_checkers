821b2dab9fa1aee7501d149d777c2395e78bff75
Janderson Lima Costa <167913806+jandersoncosta97@users.noreply.github.com>
2024-07-29 16:56:59 -0300

    versao inacabada

--- src/neural_network/yolo_test.py
+++ src/neural_network/yolo_test.py
@@ -1,10 +1,13 @@
 from model import Model
-
+import cv2
 
 def main():
     model = Model()
-    img_path = 'images/test_image.jpg'
-    model.predict(image_path=img_path)
+    img_path = 'images/images_extra_4-20240729T115001Z-001/images_extra_4/image_2.png'
+    img_read = cv2.imread(img_path)
+    img = cv2.flip(img_read, 1)
+    model.predict_from_opencv(img)
+    # model.predict(image_path=img_path)
 
 
 if __name__ == '__main__':
