f86e3f0715e3170e1ed005a7ddec55993e1a4ae5
Janderson Lima Costa <167913806+jandersoncosta97@users.noreply.github.com>
2024-07-31 15:57:40 -0300

    coordenadas ajustadas

--- src/neural_network/yolo_test.py
+++ src/neural_network/yolo_test.py
@@ -6,7 +6,8 @@ def main():
     img_path = 'images/image_2.png'
     img_read = cv2.imread(img_path)
     img = cv2.rotate(img_read, cv2.ROTATE_90_CLOCKWISE)
-    return model.predict_from_opencv(img)
+    dict_yolo = model.predict_from_opencv(img)
+    print(dict_yolo)
 
 
 if __name__ == '__main__':
